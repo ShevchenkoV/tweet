839b1f457c0adbfc0905b8bf5cc1d64629444610
ShevchenkoV <elanceShevchenko@gmail.com>
2015-10-15 14:25:44 +0300

    orderBy change

--- controllers.js
+++ controllers.js
@@ -6,34 +6,28 @@ app.controller('TwitterController', function($scope, $q, twitterService) {
     $scope.connectButton = function() {
         twitterService.connectTwitter().then(function() {
             if (twitterService.isReady()) {
-                    $scope.search();
-                    $scope.connectedTwitter = true;
+                $scope.search();
+                $scope.connectedTwitter = true;
             }
         });
     }
 
     $scope.signOut = function() {
-          twitterService.clearCache();
-          $scope.tweets.length = 0;
-          $scope.connectedTwitter = false
+      twitterService.clearCache();
+      $scope.tweets.length = 0;
+      $scope.connectedTwitter = false
          
     }
     $scope.search = function(){
       twitterService.search($scope.searchTerm).then(function(data){
         $scope.tweets = data;
-        console.log($scope.tweets);
       });
     }
+
   	$scope.$watch('check',function(newVal){
-  		if(newVal){
   			$scope.tweets.sort(function(a, b) {
-                    return b.retweet_count - a.retweet_count;
+            return  newVal==true? b.retweet_count - a.retweet_count : a.retweet_count - b.retweet_count;
           });		
-  		} else {
-  			$scope.tweets.sort(function(a, b) {
-                    return a.retweet_count - b.retweet_count;
-          });		
-  				
-  		}	
-  	})
+  	});
+
 });
