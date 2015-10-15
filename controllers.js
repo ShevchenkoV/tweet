8ec21450a0b7c72987b11907352004bfc3ca9df4
ShevchenkoV <elanceShevchenko@gmail.com>
2015-10-15 14:17:40 +0300

    cleanup

--- controllers.js
+++ controllers.js
@@ -3,7 +3,6 @@ app.controller('TwitterController', function($scope, $q, twitterService) {
     $scope.limit = 5;
     twitterService.initialize();
 
-    //when the user clicks the connect twitter button, the popup authorization window opens
     $scope.connectButton = function() {
         twitterService.connectTwitter().then(function() {
             if (twitterService.isReady()) {
@@ -13,7 +12,6 @@ app.controller('TwitterController', function($scope, $q, twitterService) {
         });
     }
 
-    //sign out clears the OAuth cache, the user will have to reauthenticate when returning
     $scope.signOut = function() {
           twitterService.clearCache();
           $scope.tweets.length = 0;
@@ -26,21 +24,16 @@ app.controller('TwitterController', function($scope, $q, twitterService) {
         console.log($scope.tweets);
       });
     }
-	$scope.$watch('check',function(newVal){
-		if(newVal){
-			$scope.tweets.sort(function(a, b) {
-                  return b.retweet_count - a.retweet_count;
-                });		
-		} else {
-			$scope.tweets.sort(function(a, b) {
-                  return a.retweet_count - b.retweet_count;
-                });		
-				
-		}	
-	})
-    //if the user is a returning user, hide the sign in button and display the tweets
-    if (twitterService.isReady()) {
-        $scope.connectedTwitter = true;
-        $scope.search();
-    }
+  	$scope.$watch('check',function(newVal){
+  		if(newVal){
+  			$scope.tweets.sort(function(a, b) {
+                    return b.retweet_count - a.retweet_count;
+          });		
+  		} else {
+  			$scope.tweets.sort(function(a, b) {
+                    return a.retweet_count - b.retweet_count;
+          });		
+  				
+  		}	
+  	})
 });
